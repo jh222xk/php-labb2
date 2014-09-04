f3e2df571b8f37b43d73e54b283229c9132f7cd3
Jesper Håkansson <jh222xk@student.lnu.se>
2014-09-04 12:25:30 +0200

    Added the User model

--- src/model/User.php
+++ src/model/User.php
@@ -4,4 +4,56 @@ namespace model;
 
 class User {
 
+  /**
+   * The users unique ID
+   * @var Integer Example 123
+   */ 
+  private $userID;
+
+  /**
+   * Username of the user
+   * @var String Example Edith
+   */
+  private $username;
+
+  /**
+   * Password for the user
+   * @var String Example 3ew0gX8V7hlS
+   */
+  private $password;
+
+  /**
+   * @param Integer $userID
+   * @param String $username
+   * @param String $password
+   */
+  public function __construct($userID, $username, $password) {
+    $this->userID = $userID;
+    $this->username = $username;
+    $this->password = $password;
+  }
+
+  /**
+   * Gets the users id
+   * @return Integer
+   */
+  public function getUserID() {
+    return $this->userID;
+  }
+
+  /**
+   * Gets the users name
+   * @return String
+   */
+  public function getUsername() {
+    return $this->username;
+  }
+
+  /**
+   * Gets the users password
+   * @return String
+   */
+  public function getPassword() {
+    return $this->password;
+  }
 }
